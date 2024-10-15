9d9f3806e1f3d3826eecbdfc9088b3213816980b
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-10-15 17:03:06 +0000

    add error handling for the and made some changes in the home page

--- Latest_movies/views.py
+++ Latest_movies/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 
 # Ref: https://github.com/kingsleyesisi/Movie-Scraping
@@ -13,33 +12,37 @@ from bs4 import BeautifulSoup
 # List to store movie data
 latest_movies = []
 
-
+try:
 # Extracting movie data from Rotten Tomatoes
-for no in range(1,4):
-    url = f"https://www.rottentomatoes.com/browse/movies_in_theaters/sort:newest?page={no}"
-    response = requests.get(url)
-    response = response.content
-    soup = BeautifulSoup(response, 'html.parser')
-    container = soup.find_all('div', class_ = 'flex-container')
-    
-for movie in container:
-    Title = movie.find('span', class_='p--small').text.strip()
-    release = movie.find('span', class_='smaller').text.strip()
-    image_tag = movie.find('rt-img', class_='posterImage')
-    if image_tag:
-        image = image_tag['src']
-
-    latest_movies.append([Title, release, image])  # Append image along with title and release
-
-
-data = {'latest_movies': latest_movies}
+    for no in range(1,4):
+        url = f"https://www.rottentomatoes.com/browse/movies_in_theaters/sort:newest?page={no}"
+        response = requests.get(url)
+        response = response.content
+        soup = BeautifulSoup(response, 'html.parser')
+        container = soup.find_all('div', class_ = 'flex-container')
         
+    for movie in container:
+        Title = movie.find('span', class_='p--small').text.strip()
+        release = movie.find('span', class_='smaller').text.strip()
+        image_tag = movie.find('rt-img', class_='posterImage')
+        if image_tag:
+            image = image_tag['src']
+
+        latest_movies.append([Title, release, image])  # Append image along with title and release
+    data = {'latest_movies': latest_movies}
+
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 # data = {'Title': [movie[0] for movie in latest_movies],
 #          'Release_Date': [movie[1] for movie in latest_movies],
+    data = {'latest_movies': 'No data available at the moment. Please check back later.'}
 #         'image': [movie[2] for movie in latest_movies],
 #         }
 
+if not latest_movies:
+    print("No movies found.")
+    exit(1)
 def home(request):    
     print(data)  # Printing data for debugging purposes
     return render(request, 'index.html', data)
