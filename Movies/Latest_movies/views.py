dbc50939ed369d39e5ad43418c2d8af8c2a5fe86
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-10-15 16:22:47 +0000

    Resructure the directory

--- Movies/Latest_movies/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-
-# Ref: https://github.com/kingsleyesisi/Movie-Scraping
-
-def home(request):        
-    return render(request, 'index.html')
