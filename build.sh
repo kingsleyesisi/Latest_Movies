a02894e8d6de59e367ff73144c41f799a633bf39
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-10-16 16:42:31 +0000

    vercel

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#! /usr/bin/env bash
+
+echo "Building Project packages..."
+python3 -m pip install -r requirements.txt
+
+echo "Migrating Database..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
